ebe5e6c7296d41f269f006ce53fc1fb0a8ae7273
Mike Hedman <mike@leapset.com>
2014-03-09 15:39:16 -0700

    Moving checks to an external file pair - default and optional override

--- runner.js
+++ runner.js
@@ -1,13 +1,21 @@
 var
   Listener = require('./lib/Listener').Listener,
-  config = require('./config.js').config;
+  config = require('./config').config,
+  checks = require('./checks').checks;
 
-  //can supply a custom config file to override the publicly visible file
-  try {
-    config = require('./custom_config.js').custom_config;
-  } catch (e) {
-    //just eat errors, this i
-  }
+//can supply a custom config file to override the publicly visible file
+try {
+  config = require('./custom_config.js').custom_config;
+} catch (e) {
+  //just eat errors, this is optional
+}
 
-var listener = new Listener(config);
+//can (should!) supply a custom config file to override the publicly visible file
+try {
+  checks = require('./custom_checks.js').custom_checks;
+} catch (e) {
+  //just eat errors, this is optional
+}
+
+var listener = new Listener(config, checks);
 listener.go();
